477565555d97a6a14e048edb543383665bba5ea8
Mahalakshmi V <vmahalakshmi940@gmail.com>
2026-03-17 12:10:55 +0530

    Analytical_Queries.sql

--- week1/Analytical_Queries.sql
+++ week1/Analytical_Queries.sql
@@ -1,9 +1,11 @@
 USE online_sales_db;
 Go
+    
 -- 1?? Total Sales
 SELECT SUM(amount) AS Total_Sales
 FROM Fact_Sales;
 GO
+    
 -- 2?? Sales by Date
 SELECT 
     d.order_date,
@@ -13,6 +15,7 @@ JOIN Dim_Date d ON f.date_id = d.date_id
 GROUP BY d.order_date
 ORDER BY d.order_date;
 Go
+    
 -- 3?? Sales by Product
 SELECT 
     p.product_id,
@@ -22,6 +25,7 @@ JOIN Dim_Product p ON f.product_id_key = p.product_id_key
 GROUP BY p.product_id
 ORDER BY Total_Sales DESC;
 Go
+    
 -- 4?? Top Selling Product
 SELECT TOP 1
     p.product_id,
@@ -31,6 +35,7 @@ JOIN Dim_Product p ON f.product_id_key = p.product_id_key
 GROUP BY p.product_id
 ORDER BY Total_Sales DESC;
 Go
+    
 -- 5?? Average Daily Sales
 SELECT 
     AVG(Daily_Sales) AS Avg_Daily_Sales
